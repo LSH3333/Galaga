be2000c1d9daec7ece83c4baab02dab72b8be33e
LSH3333 <dltpgustpgus@gmail.com>
2022-08-14 12:48:30 +0900

    bezier

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    public LineRenderer line;
-    public Transform p1, p2, p3;
     private float t;
     public GameObject obj;
+
+    public Transform[] pointsPos;
+
+
 
     // (x1,y1) (x2,y2) 내분점 위치 계산
     private Vector3 GetPos(Vector3 p1, Vector3 p2, float m)
@@ -25,28 +27,62 @@ public class Controller : MonoBehaviour
     }
 
 
-    // 두 점을 잇는 선분 위의 점 이동시킴 
-    private void MovePosition(Transform p1, Transform p2, int lineIdx)
+    // 두 점 p1, p2을 잇는 선분 위의 점 t값에 따라 이동시킨 지점 리턴 
+    private Vector3 MovePosition(Vector3 p1, Vector3 p2)
     {
-        Vector3 newPos = GetPos(p1.position, p2.position, t);
-        if (newPos.x > p2.position.x && newPos.y > p2.position.y)
+        Vector3 newPos = GetPos(p1, p2, t);
+        if (newPos.x > p2.x && newPos.y > p2.y)
         {
             t = 0;
+        }
+
+        return GetPos(p1, p2, t);
+    }
+
+    // points에 담겨있는 점들 사이의 내분점 위치를 t값에 따라 구함
+    // 구한 내분점들은 newPoints에 담김, 결과적으로 points-1개의 새로운 내분점들이 생김 
+    private void MakeBezier(int curPoint, ref List<Vector3> points, ref List<Vector3> newPoints)
+    {
+        if (curPoint >= points.Count)
+        {
+
             return;
         }
 
-        line.SetPosition(lineIdx, GetPos(p1.position, p2.position, t));
+        newPoints.Add(MovePosition(points[curPoint], points[curPoint]));
+        MakeBezier(curPoint + 1, ref points, ref newPoints);
+    }
+
+    private Vector3 dfs()
+    {
+        // 선분 위의 점들 
+        List<Vector3> points = new List<Vector3>();
+        foreach (var x in pointsPos) points.Add(x.position);
+
+        int cnt = 0;
+        // 선분 위의 점들의 갯수가 1이 될때까지 반복함
+        while (true)
+        {
+            List<Vector3> newPoints = new List<Vector3>();
+            MakeBezier(0, ref points, ref newPoints);
+            // 새롭게 구한 내분점들이 이제 기준점이됨 
+            points = newPoints;
+            if (newPoints.Count <= 1)
+                return newPoints[0];
+
+            if (cnt++ > 10) return newPoints[0];
+        }
+
     }
 
     private void Update()
     {
         t += 0.005f;
 
-        MovePosition(p1, p2, 0);
-        MovePosition(p2, p3, 1);
+        Vector3 ret = dfs();
+        print(ret);
+        obj.transform.position = ret;
 
-        obj.transform.position = GetPos(line.GetPosition(0), line.GetPosition(1), t);
-        
     }
 
 }
