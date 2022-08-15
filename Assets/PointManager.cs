ef2396bef6295089ea2058e6a39b8a88972aaa00
LSH <dltpgustpgus@gmail.com>
2022-08-15 22:05:37 +0900

    네번째 점으로 이동 안하는 문제 해결 필요

---
+++ Assets/PointManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PointManager : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    public GameObject prevPoint;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+
+        lineRenderer.SetPosition(0, gameObject.transform.position);
+        lineRenderer.SetPosition(1, prevPoint.transform.position);
+    }
+
+
+}
