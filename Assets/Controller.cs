08484896b4f73587f49f9f74bf5f69a8bc6a4912
LSH <dltpgustpgus@gmail.com>
2022-08-15 21:16:41 +0900

    bezier 수정 완료

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    // 증가되는 t 값 
     private float t;
+    // 움직이는 대상 
     public GameObject obj;
-
+    // 조절점들 
     public Transform[] pointsPos;
 
 
+
+    private void Start()
+    {
+        t = 0;
+    }
 
     // (x1,y1) (x2,y2) 내분점 위치 계산
     private Vector3 GetPos(Vector3 p1, Vector3 p2, float m)
@@ -20,18 +27,6 @@ public class Controller : MonoBehaviour
         float y = (m * p2.y + n * p1.y) / (m + n);
         return new Vector3(x, y, 0);
     }
-
-    private void Start()
-    {
-        t = 0;
-
-        Vector2 res = new Vector2(1, 2) - new Vector2(3, 2);
-
-        Vector2 v1 = Vector2.zero;
-        Vector2 v2 = new Vector2(3, 4);
-        
-    }
-
 
     // 두 점 p1, p2을 잇는 선분 위의 점 t값에 따라 이동시킨 지점 리턴 
     private Vector3 MovePosition(Vector3 p1, Vector3 p2)
@@ -45,50 +40,28 @@ public class Controller : MonoBehaviour
         return GetPos(p1, p2, t);
     }
 
-    // points에 담겨있는 점들 사이의 내분점 위치를 t값에 따라 구함
-    // 구한 내분점들은 newPoints에 담김, 결과적으로 points-1개의 새로운 내분점들이 생김 
-    private void MakeBezier(int curPoint, ref List<Vector3> points, ref List<Vector3> newPoints)
+    Vector3 dfs(ref List<Vector3> points)
     {
-        if (curPoint >= points.Count)
+        // 점이 하나 남으면 종료 
+        if (points.Count == 1) return points[0];
+
+        List<Vector3> newPoints = new List<Vector3>(); 
+        for (int i = 0; i < points.Count - 1; i++)
         {
-
-            return;
+            newPoints.Add(MovePosition(points[i], points[i + 1]));
         }
-
-        newPoints.Add(MovePosition(points[curPoint], points[curPoint]));
-        MakeBezier(curPoint + 1, ref points, ref newPoints);
-    }
-
-    private Vector3 dfs()
-    {
-        // 선분 위의 점들 
-        List<Vector3> points = new List<Vector3>();
-        foreach (var x in pointsPos) points.Add(x.position);
-
-        int cnt = 0;
-        // 선분 위의 점들의 갯수가 1이 될때까지 반복함
-        while (true)
-        {
-            List<Vector3> newPoints = new List<Vector3>();
-            MakeBezier(0, ref points, ref newPoints);
-            // 새롭게 구한 내분점들이 이제 기준점이됨 
-            points = newPoints;
-            if (newPoints.Count <= 1)
-                return newPoints[0];
-
-            if (cnt++ > 10) return newPoints[0];
-        }
-
+        return dfs(ref newPoints);
     }
 
     private void Update()
     {
-        t += 0.005f;
+        t += 0.0015f;
 
-        Vector3 ret = dfs();
-        print(ret);
-        obj.transform.position = ret;
+        List<Vector3> points = new List<Vector3>();
+        foreach (var x in pointsPos) points.Add(x.position);
+        Vector3 newPoint = dfs(ref points);
 
+        obj.transform.position = newPoint;
     }
 
 }
