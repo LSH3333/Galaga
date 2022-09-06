cd54183b2cf85bf0631fa8773d6bd86614c63c18
LSH3333 <dltpgustpgus@gmail.com>
2022-09-06 16:56:45 +0900

    Test scene bullet 쿨타임 관련 작업 필요

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	public float bulletSpeed = 5f;
+	private Rigidbody2D rb;
+	private Vector3 movement;
+	
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		movement = new Vector2(0f, 1f);
+	}
+
+    private void Update()
+    {
+		
+    }
+
+    void FixedUpdate()
+	{
+
+		// 좌우 이동 
+		rb.MovePosition(transform.position + (movement * bulletSpeed * Time.deltaTime));		
+	}
+
+
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+	public float speed = 20f;
+	private Rigidbody2D rb;
+	private Vector3 movement;
+	public GameObject bullet;
+
+	private float bulletCoolTime;
+	private float bulletCoolSpeed = 4f;
+	private int bulletCnt;
+
+	private void Awake()
+    {
+		rb = GetComponent<Rigidbody2D>();
+    }
+
+	bool startTime = false;
+	bool canFire = true;
+    void Update()
+	{		
+		// move input
+		movement = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+
+		
+		// bullet 
+		if (Input.GetKeyDown("space"))
+        {
+			if (canFire)
+			{
+				Instantiate(bullet, gameObject.transform.position, Quaternion.identity);
+				bulletCnt++;
+			}
+		}
+		print(bulletCoolTime);
+		if (bulletCnt == 1) startTime = true;
+		if (startTime) bulletCoolTime += Time.deltaTime * bulletCoolSpeed;
+		if (bulletCoolTime < 1 && bulletCnt >= 2) canFire = false;
+		if (bulletCoolTime >= 1) { bulletCoolTime = 0; canFire = true; startTime = false; }
+	}
+
+	void FixedUpdate()
+	{		
+
+		// 좌우 이동 
+		rb.MovePosition(transform.position + (movement * speed * Time.deltaTime));
+	}
+
+
+
+}
