7ebf8a38557a590637dbee138015d40c4b142456
LSH <dltpgustpgus@gmail.com>
2022-09-24 22:26:37 +0900

    1 Level Pattern 구현 완료, 적 모두 자리 찾아가고 공격 로직 구현 필요

--- Assets/Scripts/PatternInfo.cs
+++ Assets/Scripts/PatternInfo.cs
@@ -5,7 +5,6 @@ public class PatternInfo : MonoBehaviour
 {
     // true시 Objs의 x좌표 반전해서  MirrorObjs 소환 
     public bool mirror; 
-    public int enemyCnt;
     public GameObject ControlPoint;
     public GameObject Objs;
 }
