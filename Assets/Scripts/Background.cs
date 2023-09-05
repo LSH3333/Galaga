bc8f32e6a36b0d64f6d1bcd040abd1c9466446b3
LSH <dltpgustpgus@gmail.com>
2023-09-05 14:20:21 +0900

    background 적용 완료

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    private Rigidbody2D _rb2d;
+
+    private void Start()
+    {
+        setRigidbody(2f);
+    }
+
+    public void setRigidbody(float speed)
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+        _rb2d.bodyType = RigidbodyType2D.Kinematic;
+        _rb2d.velocity = new Vector2(0f, -speed);
+    }
+
+    public void setStop()
+    {
+        _rb2d.velocity = Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/VRepeat.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class VRepeat : MonoBehaviour
+{
+    private BoxCollider2D _box;
+    public float _verticalLength;
+
+    private void Awake()
+    {
+        setBoxCollider();
+    }
+
+    private void Update()
+    {
+        updateObject();
+    }
+
+    public void setBoxCollider()
+    {
+        _box = GetComponent<BoxCollider2D>();
+        _verticalLength = _box.size.y;
+        
+    }
+
+    public void updateObject()
+    {
+        if (-transform.position.y > _verticalLength)
+        {
+            ResetPosition();
+        }            
+    }
+
+    private void ResetPosition()
+    {
+        Vector2 addPos = new Vector2(0f, 2 * _verticalLength);
+        transform.position = (Vector2)transform.position + addPos;
+    }
+}
